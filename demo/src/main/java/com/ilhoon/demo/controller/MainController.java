e00ed3749862e973e5fd5de908f8f1e890305509
1hoon <chiwoo2074@gmail.com>
2020-05-31 12:13:35 +0900

    jsp연동누락분

--- demo/src/main/java/com/ilhoon/demo/controller/MainController.java
+++ demo/src/main/java/com/ilhoon/demo/controller/MainController.java
@@ -2,6 +2,7 @@ package com.ilhoon.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController
@@ -10,5 +11,15 @@ public class MainController
     public String main ()
     {
         return "index.html";
+    }
+
+    @RequestMapping("/jsp")
+    public ModelAndView jsp ()
+    {
+        ModelAndView model = new ModelAndView("main");
+
+        model.addObject("testvalue", "안녕!");
+
+        return model;
     }
 }
--- demo/src/main/java/com/ilhoon/demo/controller/RestController.java
+++ demo/src/main/java/com/ilhoon/demo/controller/RestController.java
@@ -3,6 +3,11 @@ package com.ilhoon.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * @RestController 란?
+ * @Controller + @ResponseBody
+ * JSON 또는 XML 형태의 데이터 반환시 사용. API 서버로 활용시 많이 사용한다.
+ */
 @org.springframework.web.bind.annotation.RestController
 public class RestController
 {
