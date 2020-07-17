94cc0cad2c983d91566c91d02fe4907a1e662cf4
1hoon <chiwoo2074@gmail.com>
2020-07-17 22:10:23 +0900

    Spring Security OAuth Server Config

---
+++ spring-boot-rest-api/src/test/java/org/hoon/springbootrestapi/config/AuthServerConfigTest.java
@@ -0,0 +1,50 @@
+package org.hoon.springbootrestapi.config;
+
+import org.hoon.springbootrestapi.account.Account;
+import org.hoon.springbootrestapi.account.AccountRole;
+import org.hoon.springbootrestapi.account.AccountService;
+import org.hoon.springbootrestapi.common.BaseControllerTest;
+import org.hoon.springbootrestapi.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest
+{
+	@Autowired
+	AccountService accountService;
+
+	@Test
+	@TestDescription("인증 토큰을 발급 받는 테스트")
+	public void getAuthToken() throws Exception
+	{
+		// 테스트 계정 생성
+		String username = "2hoon";
+		String password = "1234";
+		Account account = Account.builder()
+									.email(username)
+									.password(password)
+									.roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+								.build();
+		this.accountService.saveAccount(account);
+
+		String clientId = "myApp";
+		String clientSecret = "pass";
+
+		this.mockMvc.perform(post("/oauth/token")
+								.with(httpBasic(clientId, clientSecret))
+								.param("username", username)
+								.param("password", password)
+								.param("grant_type", "password"))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("access_token").exists());
+	}
+}
